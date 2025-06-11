b5249069ddcf0d1164a0c9c23cb7f07f72daac0e
kezri29 <kesari.2911@gmail.com>
2025-06-11 16:54:05 +0530

    testing16 for the new mcp server agent

--- main/LibraryManagementSystem.java
+++ main/LibraryManagementSystem.java
@@ -1,30 +1,7 @@
-import java.util.*;
-
-class booklib{
-String T;String A;boolean isB=false;
-booklib(String t,String a){T=t;A=a;}
-public String toString(){return T+" by "+A+(isB?" [BORROWED]":"");}
+public class Example {
+public static void main(String[] a){
+doit();
 }
 
-class libMan{
-ArrayList<booklib> bList=new ArrayList<>();
-void aB(String t,String a){bList.add(new booklib(t,a));}
-void sB(){for(int i=0;i<bList.size();i++){System.out.println(i+"->"+bList.get(i));}}
-void bB(int id){if(id>=0&&id<bList.size()){if(!bList.get(id).isB){bList.get(id).isB=true;System.out.println("yay! u got it");}else System.out.println("nope taken");}else System.out.println("wat?");}
-void rB(int id){if(id>=0&&id<bList.size()){if(bList.get(id).isB){bList.get(id).isB=false;System.out.println("ty 4 return");}else System.out.println("it wasn’t out");}else System.out.println("wat?");}
-}
-
-public class LibraryManagementSystem {
-public static void main(String argz[]){
-libMan l=new libMan();
-l.aB("1984","George");
-l.aB("HP1","Rowling");
-l.aB("Sapiens","Harari");
-Scanner sc=new Scanner(System.in);int ch=0;
-System.out.println("Welcome 2 lib");while(true){
-System.out.println("1.show 2.borrow 3.return 9.quit");try{ch=Integer.parseInt(sc.nextLine());}catch(Exception e){System.out.println("num pls");continue;}
-if(ch==1)l.sB();else if(ch==2){System.out.print("ID?");int i=Integer.parseInt(sc.nextLine());l.bB(i);}
-else if(ch==3){System.out.print("ID?");int i=Integer.parseInt(sc.nextLine());l.rB(i);}
-else if(ch==9){System.out.println("bbye");break;}else System.out.println("no idea what u want");}
-}
-}
+public static void doit(){
+int x=10;int y=20;int z=30;if(x<y){System.out.println("x is less than y");}for(int i=0;i<5;i++){System.out.println("i is "+i);}if(z>y){System.out.println("z is big");}else{System.out.println("y is big");}String n="This is a very long string which doesn't need to be here and makes no sense but it's still here just because";System.out.println(n);int res = x + y + z + 100 - 50 * 2;System.out.println("Result is: "+res);if(res>100){res=res/2;}System.out.println("Final Result: "+res);}}
