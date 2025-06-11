fbd8ff899a51ce6f55072a30bd1b508d2a2ca62b
kezri29 <kesari.2911@gmail.com>
2025-06-11 21:59:38 +0530

    testing24 for the new mcp server agent

---
+++ main/new.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public static void main(String[] args) {
+        calc(5, 0, "divide");
+    }
+
+    public static void calc(int a, int b, String op) {
+    if(op=="add"){System.out.println("Result="+(a+b));}
+    else if(op=="sub"){System.out.println("Result="+(a-b));}
+    else if(op=="multiply"){System.out.println("Result="+(a*b));}
+    else if(op=="divide"){System.out.println("Result="+(a/b));}
+    else{System.out.println("wrong op");}
+    System.out.println("End of calculation");
+    System.out.println("Thank you for using the calculator!");
+    System.out.println("Have a great day!");
+    System.out.println("Goodbye!");
+    System.out.println("Hi there");
+    System.out.println("Welcome to the calculator program!");
+    }
+}
