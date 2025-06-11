5dfbe731f9eeb8913027df6be2ce53f19abf3922
kezri29 <kesari.2911@gmail.com>
2025-06-11 17:01:34 +0530

    testing17 for the new mcp server agent

--- main/LibraryManagementSystem.java
+++ main/LibraryManagementSystem.java
@@ -1,7 +1,13 @@
-public class Example {
-public static void main(String[] a){
-doit();
-}
+public class Calculator {
+    public static void main(String[] args) {
+        calc(5, 0, "divide");
+    }
 
-public static void doit(){
-int x=10;int y=20;int z=30;if(x<y){System.out.println("x is less than y");}for(int i=0;i<5;i++){System.out.println("i is "+i);}if(z>y){System.out.println("z is big");}else{System.out.println("y is big");}String n="This is a very long string which doesn't need to be here and makes no sense but it's still here just because";System.out.println(n);int res = x + y + z + 100 - 50 * 2;System.out.println("Result is: "+res);if(res>100){res=res/2;}System.out.println("Final Result: "+res);}}
+    public static void calc(int a, int b, String op) {
+    if(op=="add"){System.out.println("Result="+(a+b));}
+    else if(op=="sub"){System.out.println("Result="+(a-b));}
+    else if(op=="multiply"){System.out.println("Result="+(a*b));}
+    else if(op=="divide"){System.out.println("Result="+(a/b));}
+    else{System.out.println("wrong op");}
+    }
+}
