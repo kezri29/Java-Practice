97c8b906d93d0d90d55cee14cc1c56071aa67a3a
Kesarikumaran S <kesarikumaran.s@prodapt.com>
2025-06-12 11:28:48 +0530

    testing26 for the pr agent

---
+++ main/new4.java
@@ -0,0 +1,1 @@
+class a{public static void main(String b[]){int i=0,j=0,t,n[]={5,1,4,2,8};for(i=0;i<n.length-1;i++){for(j=0;j<n.length-i-1;j++){if(n[j]>n[j+1]){t=n[j];n[j]=n[j+1];n[j+1]=t;}}}for(i=0;i<5;i++){System.out.print(n[i]+" ");}}}
